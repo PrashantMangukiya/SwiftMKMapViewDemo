0faaf2c82830b590a6ca1a96fe066a47a1477499
PrashantKumar Mangukiya <pbMangukiya@gmail.com>
2015-10-28 12:39:26 +0530

    Update Source: User Location, Zoom In/Out  functionality added.
    Source code updated, 1) added functionality for showing user location
    on map when location button clicked at top right. 2) Map Zoom In / out
    functionality added when clicked zoom button.

--- SwiftMKMapViewDemo/SwiftMKMapViewDemo/ViewController.swift
+++ SwiftMKMapViewDemo/SwiftMKMapViewDemo/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    
+    // location manager object
+    let locationManager = CLLocationManager()
+    
     
     // outlet and action - map type segment control
     @IBOutlet var mapType: UISegmentedControl!
@@ -31,26 +36,124 @@ class ViewController: UIViewController {
             self.mapType.tintColor = UIColor.whiteColor()
             self.myMapView.mapType = MKMapType.Hybrid
         }
-        
     }
     
-    // outlet - UI map view
+    
+    // outlet - map view
     @IBOutlet var myMapView: MKMapView!
     
-    
+    // action - user location button
+    @IBAction func userLocationButtonAction(sender: UIBarButtonItem) {
         
+        // show user location on map (it will show blue dot at user location)
+        self.myMapView.showsUserLocation = true
+        
+        // start updating location, so it will refresh map region with recent location.
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    // action - zoom in button
+    @IBOutlet var zoomInButton: UIBarButtonItem!
+    @IBAction func zoomInButtonAction(sender: UIBarButtonItem) {
+        
+        // create span
+        let deltaX = self.myMapView.region.span.latitudeDelta - 0.001
+        let deltaY = self.myMapView.region.span.longitudeDelta - 0.001
+        let newSpan = MKCoordinateSpanMake(deltaX, deltaY)
+        
+        // find user location
+        let userLocation = self.myMapView.userLocation
+        
+        // create new region
+        let newRegion = MKCoordinateRegion(center: userLocation.coordinate, span: newSpan)
+        
+        // set segion within map
+        self.myMapView.setRegion(newRegion, animated: true)
+        
+        // if enough zoon in then disable zoom in button
+        if deltaX < 0.001 {
+            self.zoomInButton.enabled = false
+        }        
+    }
+    
+    // action - zoom out button
+    @IBOutlet var zoomOutButton: UIBarButtonItem!
+    @IBAction func zoomOutButtonAction(sender: UIBarButtonItem) {
+        
+        // create span
+        let deltaX = self.myMapView.region.span.latitudeDelta + 0.001
+        let deltaY = self.myMapView.region.span.longitudeDelta + 0.001
+        let newSpan = MKCoordinateSpanMake(deltaX, deltaY)
+        
+        // find user location
+        let userLocation = self.myMapView.userLocation
+        
+        // create new region
+        let newRegion = MKCoordinateRegion(center: userLocation.coordinate, span: newSpan)
+        
+        // set segion within map
+        self.myMapView.setRegion(newRegion, animated: true)
+        
+        // enable zoon in button when delta increase enough
+        if deltaX > 0.002 {
+           self.zoomInButton.enabled = true
+        }
+    }
+    
+    
+    
     // MARK: - View functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        // set location manager delegate
+        self.locationManager.delegate = self
+        
+        // set map view delegate
+        self.myMapView.delegate = self
+        
+        // set location accuracy
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // request location access permision from user.
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        // start updating location, so it will set map region initially
+        self.locationManager.startUpdatingLocation()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    // MARK: - location manager delegate
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        
+        // create span
+        let deltaX = 0.01
+        let deltaY = 0.01
+        let newSpan = MKCoordinateSpanMake(deltaX, deltaY)
+        
+        // create new region
+        let newRegion = MKCoordinateRegion(center: newLocation.coordinate, span: newSpan)
+        
+        // set new region for map
+        self.myMapView.setRegion(newRegion, animated: true)
+
+        // stop updating location 
+        // we have to update region once so no need to keep updating location
+        self.locationManager.stopUpdatingLocation()
+        
+        // enable zoom in button if disabled.
+        self.zoomInButton.enabled = true
+    }
+    
 
 }
 
